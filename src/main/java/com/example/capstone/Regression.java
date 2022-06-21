5de30508809a7e88403c14f51c675e4b97792d20
earthafire <resiget@gmail.com>
2022-06-20 20:43:32 -0700

    added axes and regression accuracy test

--- src/main/java/com/example/capstone/Regression.java
+++ src/main/java/com/example/capstone/Regression.java
@@ -15,6 +15,33 @@ public class Regression {
         }
 
         Double prediction = R.predict(yearExperience);
+        //testPercentageDifference(rows, R);
         return prediction;
+    }
+
+    public static void testPercentageDifference(ArrayList<Row> rows, SimpleRegression R){
+        final int testToYear = 14;
+        int[] totalsalary = new int[testToYear];
+        int[] datapoints = new int[testToYear];
+        for(int i = 0; i < testToYear; i++){
+            totalsalary[i] = 0;
+            datapoints[i] = 0;
+        }
+        for(Row row: rows){
+            if(row.getYearsExperience() < testToYear){
+                totalsalary[row.getYearsExperience()] += row.getConvertedCompensation();
+                datapoints[row.getYearsExperience()]++;
+            }
+        }
+        double successRate = 0;
+        for(int i = 1; i < testToYear; i++){
+            System.out.println("YOE: " + i + " Prediction: " + R.predict(i) + " Average: " + totalsalary[i]/datapoints[i]);
+            double percDif = (((1.0 * totalsalary[i]/datapoints[i]) - R.predict(i)) / (1.0 * totalsalary[i]/datapoints[i]));
+            System.out.println("Percentage Difference: " + percDif * 100);
+            if(percDif < .1 && percDif > -.1){
+                successRate++;
+            }
+        }
+        System.out.println(successRate/testToYear * 100 + "% Success Rate");
     }
 }
